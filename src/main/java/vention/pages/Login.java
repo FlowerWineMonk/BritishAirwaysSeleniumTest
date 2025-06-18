0628abd5f8030d020c51df631e85598bfc3e2fea
FlowerWineMonk <hidekitakuya0@gmail.com>
2025-06-18 12:57:28 +0500

    Add Login page

--- src/main/java/vention/pages/Login.java
+++ src/main/java/vention/pages/Login.java
@@ -1,1 +1,29 @@
-// Login + Registration
+package vention.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage extends AbstractPage {
+  private static final String LOGIN_BUTTON = "//button[@data-testid='login-button']";
+  private static final Strign REGISTER_BUTTON = "//button[@data-testid='register-button']";
+
+  @FindBy(xpath = LOGIN_BUTTON)
+  private WebElement LoginBtn;
+
+  @FindBy(xpath = REGISTER_BUTTON)
+  private WebElement RegisterBtn;
+
+  public LoginPage() {
+    super();
+    PageFactory.initElements(driver, this);
+  }
+
+  public void LoginBtnClick() {
+    waitForVisibilityAndClick(LoginBtn);
+  }
+
+  public void RegisterBtnClick() {
+    waitForVisibilityAndClick(RegisterBtn);
+  }
+}
