57ab5d04183053462586e3ee42c66a1faffa4fe6
FlowerWineMonk <hidekitakuya0@gmail.com>
2025-06-23 11:47:27 +0500

    Added softAsserts and postcondition to verify the url

--- src/test/java/vention/pagesTest/LoginPageTest.java
+++ src/test/java/vention/pagesTest/LoginPageTest.java
@@ -1,21 +1,31 @@
 package vention.pagesTest;
 
 import vention.configLoader.ConfigLoader;
+import vention.driver.DriverManager;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class LoginPageTest extends BaseTest {
   @Test
   public void testLoginPageFunctionality() {
-    Assert.assertTrue(loginPage.isEmailFieldDisplayed(), "Email field should be visible");
-    Assert.assertTrue(loginPage.isPasswordFieldDisplayed(), "Password field should be visible");
-    //Assert.assertTrue(loginPage.isCaptchaFieldDisplayed(), "Captcha field should be visible");
-    Assert.assertTrue(loginPage.isNextBtnDisplayed(), "Next button should be visible");
+    SoftAssert softAssert = new SoftAssert();
+
+    softAssert.assertTrue(loginPage.isEmailFieldDisplayed(), "Email field should be visible");
+    softAssert.assertTrue(loginPage.isPasswordFieldDisplayed(), "Password field should be visible");
+    softAssert.assertTrue(loginPage.isNextBtnDisplayed(), "Next button should be visible");
 
     String email = ConfigLoader.get("email");
     String password = ConfigLoader.get("password");
+
     loginPage.fillLoginCredentials(email, password);
-    //loginPage.clickCaptchaField();
     loginPage.clickNextBtn();
+
+    String expectedUrl = ConfigLoader.get("home-url");
+    String actualUrl = DriverManager.getDriver().getCurrentUrl();
+
+    softAssert.assertEquals(actualUrl, expectedUrl, "User should be redirected to the home page afrer login");
+
+    softAssert.assertAll();
   }
 }
